64f05e00a9081b6cbe23b9cf7cf12f901e84122e
Sulimovskyy Vladyslav <sulimovskyy.vladyslav@gmail.com>
2018-01-15 14:37:42 +0100

    ger reporting by id

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var envConfig = require("./config/env.config");
 var mongoose = require("mongoose");
 var app = require("express")();
 
-mongoose.connect(databaseConfig.database);
+mongoose.connect(databaseConfig.database, databaseConfig.databaseOptions);
 mongoose.connection.on("error", function (errorWithMongo) {
     console.error("MongoDB error: " + errorWithMongo.message);
     process.exit(3);
 });
-
 
 require("./config/express")(app);
 require("./routes")(app);
