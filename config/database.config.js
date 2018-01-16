8eb94bdb9f90c491d695298c029f8dd1d4d8993f
Sulimovskyy Vladyslav <sulimovskyy.vladyslav@gmail.com>
2018-01-16 16:50:13 +0100

    Database config

--- config/database.config.js
+++ config/database.config.js
@@ -1,13 +1,13 @@
 module.exports =
     {
         //String used to connect to mongo.
-    "database": "mongodb://<user>.<pw>@ds044787.mlab.com:44787/unicam_ids_db",
+    "database": "mongodb://<ID>:<PW>@ds044787.mlab.com:44787/unicam_ids_db",
         //Database options.
         "databaseOptions": {
             "server": {
                 "socketOptions": {
-                    "keepAlive": 666,
-                    "connectTimeoutMS": 666
+                    "keepAlive": true,
+                    "connectTimeoutMS": 2000
                 }
             }
         }
